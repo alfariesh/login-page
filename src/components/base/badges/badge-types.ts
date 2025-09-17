a15d63943d493f0488cf1292e3d7eaaab2cfcf74
Your Name <your.email@example.com>
2025-09-17 14:04:27 +0700

    initial

---
+++ src/components/base/badges/badge-types.ts
@@ -0,0 +1,13 @@
+export type BadgeColors =
+    | "primary"
+    | "secondary"
+    | "success"
+    | "warning"
+    | "error"
+    | "info"
+    | "neutral"
+    | "brand";
+
+export type BadgeSize = "sm" | "md" | "lg";
+
+export type BadgeVariant = "solid" | "outline" | "subtle";
